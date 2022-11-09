62888d49f660cb52471408f2064ecc1eadf54f62
Ucheizzzy <uchechukwuobiakor@gmail.com>
2022-11-09 03:54:23 +0100

    Database migration ran, polling unit controllers

---
+++ resources/views/master.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Obiakor Uchechukwu Israel">
+    <meta name="description" content="Bincom Dev INEC Technical Analysis">
+    <meta name="keywords" content="Bincom Dev Center, INEC, polling unit, lgas, states, wards, party, anounced results">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bincom Dev | Results Page</title>
+    {{-- Bootstrap link --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Hello</h1>
+            </div>
+        </div>
+    </div>
+    
+    {{-- Botstrap js --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="{{asset('js/jquery.min.js')}}" type="text/javascript"></script>
+    <script>
+        $(document).ready(function(){
+            // alert('ready')
+        })
+    </script>
+</body>
+</html>
